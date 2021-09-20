1e54b88b9575e2ab8af91aa99ddb3d367ca44eb8
Imran Abdulmalik <isoteriksoftware@gmail.com>
2021-09-20 12:17:21 +0100

    configured runtime test

---
+++ runtime-test/android/src/com/isoterik/racken/test/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.isoterik.racken.test;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.isoterik.racken.test.TestDriver;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TestDriver(), config);
+	}
+}
---
+++ runtime-test/desktop/src/com/isoterik/racken/test/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.isoterik.racken.test.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.isoterik.racken.test.TestDriver;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TestDriver(), config);
+	}
+}
