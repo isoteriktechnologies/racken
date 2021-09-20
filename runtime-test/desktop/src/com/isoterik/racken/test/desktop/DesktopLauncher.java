dedd790c9266df5f733175d01acc357efe768ad7
Imran Abdulmalik <isoteriksoftware@gmail.com>
2021-09-20 12:39:40 +0100

    runtime-test config

--- runtime-test/desktop/src/com/isoterik/racken/test/desktop/DesktopLauncher.java
+++ runtime-test/desktop/src/com/isoterik/racken/test/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package com.isoterik.racken.test.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.isoterik.racken.test.TestDriver;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new TestDriver(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new TestDriver(), config);
 	}
 }
