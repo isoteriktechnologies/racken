b7a002557bf2b37944a18413da46e7fcaa2e1eed
Imran Abdulmalik <isoteriksoftware@gmail.com>
2021-09-24 07:46:39 +0100

    Update CameraTest.java

--- runtime-test/core/src/com/isoterik/racken/test/CameraTest.java
+++ runtime-test/core/src/com/isoterik/racken/test/CameraTest.java
@@ -1,6 +1,7 @@
 package com.isoterik.racken.test;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.isoterik.racken.GameObject;
 import com.isoterik.racken.Scene;
@@ -12,23 +13,26 @@ public class CameraTest extends Scene {
 
     @Override
     protected void onCreate() {
-        GameAssetsLoader gameAssetsLoader = racken.assets;
-        gameAssetsLoader.enqueueAsset("maps/tiled.tmx", TiledMap.class);
-        gameAssetsLoader.loadAssetsNow();
-        tiledMapRenderer = new TiledMapRenderer(gameAssetsLoader.getAsset("maps/tiled.tmx", TiledMap.class),
-                1/21f);
-
-        racken.defaultSettings.VIEWPORT_WIDTH = tiledMapRenderer.mapWidth/2f;
-        racken.defaultSettings.VIEWPORT_HEIGHT = tiledMapRenderer.mapHeight/2f;
-        racken.defaultSettings.PIXELS_PER_UNIT = 21;
+//        GameAssetsLoader gameAssetsLoader = racken.assets;
+//        gameAssetsLoader.enqueueAsset("maps/tiled.tmx", TiledMap.class);
+//        gameAssetsLoader.loadAssetsNow();
+//        tiledMapRenderer = new TiledMapRenderer(gameAssetsLoader.getAsset("maps/tiled.tmx", TiledMap.class),
+//                1/21f);
+//
+//        racken.defaultSettings.VIEWPORT_WIDTH = tiledMapRenderer.mapWidth/2f;
+//        racken.defaultSettings.VIEWPORT_HEIGHT = tiledMapRenderer.mapHeight/2f;
+//        racken.defaultSettings.PIXELS_PER_UNIT = 100;
     }
 
     public CameraTest() {
         setBackgroundColor(Color.BLACK);
 
-        GameObject tiledMapObject = GameObject.newInstance("TiledMap");
-        addGameObject(tiledMapObject);
-        tiledMapObject.addComponent(tiledMapRenderer);
+//        GameObject tiledMapObject = GameObject.newInstance("TiledMap");
+//        addGameObject(tiledMapObject);
+//        tiledMapObject.addComponent(tiledMapRenderer);
+
+        GameObject object = newSpriteObject(new Texture("badlogic.jpg"));
+        addGameObject(object);
     }
 }
 
